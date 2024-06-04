c06f6eb6b6ada3e055462399afefc4f1f1f7c137
BuildTools <unconfigured@null.spigotmc.org>
2024-06-03 23:50:55 -0300

    .

--- Testing/jugando con sets.py
+++ Testing/jugando con sets.py
@@ -1,17 +1,17 @@
-set1 = {1,2,3,"pepe"}
+set1 = {1, 2, 3, "pepe"}
 set1.add("jorge")
 print(set1)
-set2 = set("ejajaefdfkdgg")
+set2 = set('abcdebcerfba')
 print(set2)
-notas =  {
-    "pepe":1,
-    "jorge":2,
-    "ramiro":3,
-    "juanito":4,
-    "facundo":5,
-    "sofia":6,
-    "mateo":7,
-    "gaston":8,
+notas = {
+    "pepe": 1,
+    "jorge": 2,
+    "ramiro": 3,
+    "juanito": 4,
+    "facundo": 5,
+    "sofia": 6,
+    "mateo": 7,
+    "gaston": 8,
 }
 for k in notas:
     notas[k] = notas[k] + 1
